5a064c45afe35a946b17431704d4b68405080a71
nayon117 <hasibul.nayon1@gmail.com>
2026-01-16 09:02:52 +0600

    create noresult page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,54 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
+
+// create 5 questions array of objects with _id, title, tags which is array of objects containing _id, and name then next author, upvotes,views, answers, createdAt
+const questions = [
+//   {
+//     _id: "1",
+//     title: "How to implement authentication in Next.js?",
+//     tags: [
+//       { _id: "t1", name: "Next.js" },
+//       { _id: "t2", name: "Authentication" },
+//     ],
+//     author: "John Doe",
+//     upvotes: 10,
+//     views: 100,
+//     answers: 2,
+//     createdAt: new Date("2024-01-01"),
+//   },
+//     {
+//     _id: "2",
+//     title: "What is the difference between React and Angular?",
+//     tags: [
+//         { _id: "t3", name: "React" },
+//         { _id: "t4", name: "Angular" },
+//     ],
+//     author: "Jane Smith",
+//     upvotes: 15,
+//     views: 150,
+//     answers: 3,
+//     createdAt: new Date("2024-02-01"),
+//   },
+//   {
+//     _id: "3",
+//     title: "How to manage state in React?",
+//     tags: [
+//         { _id: "t5", name: "React" },
+//         { _id: "t6", name: "State Management" },
+//     ],
+//     author: "Alice Johnson",
+//     upvotes: 20,
+//     views: 200,
+//     answers: 5,
+//     createdAt: new Date("2024-03-01"),
+//   },
+]
 
 const HomePage = () => {
   return (
@@ -31,6 +76,15 @@ const HomePage = () => {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex flex-col w-full gap-6">
+        {questions.length > 0 ? questions.map((question)=>(
+            'QuestionCard'
+        )) : <NoResult
+        title="No Questions Found"
+        link = "/ask-question"
+        linkTitle = "Ask a Question"
+        />}
+      </div>
     </>
   );
 };
