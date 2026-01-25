7a3b716b86f34bd241bfe85d13dbc8abfba40861
nayon117 <hasibul.nayon1@gmail.com>
2026-01-25 09:31:11 +0600

    create crateAnswer server action

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,23 @@
+"use server"
+
+import Answer from "@/database/answer.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams } from "./shared.types";
+import Question from "@/database/question.model";
+import { revalidatePath } from "next/cache";
+
+export async function createAnswer (params:CreateAnswerParams) {
+    try {
+        connectToDatabase()
+        const { content, author, question, path } = params;
+        const newAnswer = new Answer({ content, author, question });
+        // add the answer to the quesitons' answer array
+        await Question.findByIdAndUpdate(question, {
+            $push: { answers: newAnswer._id }
+        })
+        // TODO: add interactions...
+        revalidatePath(path);
+    } catch (error) {
+        console.error("Database connection error:", error);
+    }
+}
